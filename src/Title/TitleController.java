25f9626299c06bd278dd0138c533ad2fa51b028c
Sam <samhudock@gmail.com>
2016-02-05 17:01:57 -0800

    Initial commit.

---
+++ src/Title/TitleController.java
@@ -0,0 +1,13 @@
+package Title;
+
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+
+public class TitleController
+{
+    @FXML
+    protected void handleStartClicked(MouseEvent event)
+    {
+        System.out.println("OK");
+    }
+}
