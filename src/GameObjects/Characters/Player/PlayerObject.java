2fa507beae2af4f6311ad1ef188a57ba8e931752
Sam <Sam@localhost>
2016-02-21 13:31:13 -0500

    Adjusting walk animation gradient.

--- src/GameObjects/Characters/Player/PlayerObject.java
+++ src/GameObjects/Characters/Player/PlayerObject.java
@@ -3,6 +3,7 @@ package GameObjects.Characters.Player;
 import Animation.SpriteAnimation;
 import GameObjectBase.GameWorldObject;
 import GameObjects.Characters.CharacterBase;
+import javafx.scene.canvas.GraphicsContext;
 
 import java.awt.*;
 
@@ -13,6 +14,23 @@ public class PlayerObject extends CharacterBase
     {
         super(size, false, mass, health,
                 new SpriteAnimation(
-                        "src/ImageAssets/player/shuf64png.png", 64, 64, 10, 60, true));
+                        "src/ImageAssets/player/shuf64png.png", 64, 64, 5, 60, true));
+    }
+
+    @Override
+    public void DrawWalkAnimation(GraphicsContext gc)
+    {
+        if (this.GetVelocity() != null
+                && this.GetVelocity().GetSpeed() != 0.0)
+        {
+            if(this.GetVelocity().GetSpeed() < 0.2)
+                _walkCycle.SetAnimationFps(2);
+            else if(this.GetVelocity().GetSpeed() < 1.0)
+                _walkCycle.SetAnimationFps(5);
+            else
+                _walkCycle.SetAnimationFps(10);
+        }
+
+        super.DrawWalkAnimation(gc);
     }
 }
