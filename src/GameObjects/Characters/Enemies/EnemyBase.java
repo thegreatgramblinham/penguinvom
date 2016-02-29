c2076a479667e6556248f8e271ab1cf086b4cb6f
Sam Hudock <samhudock@gmail.com>
2016-02-29 13:11:23 -0500

    Adding touch damage to player from enemies.

--- src/GameObjects/Characters/Enemies/EnemyBase.java
+++ src/GameObjects/Characters/Enemies/EnemyBase.java
@@ -9,14 +9,32 @@ import java.awt.*;
 
 public class EnemyBase extends CharacterBase
 {
+    //Properties
+    private int _touchDamage;
 
+    //Constructor
     public EnemyBase(Rectangle size, boolean isImmobile,
                      float mass, int health,
-                     SpriteAnimation walkCycle, SpriteAnimation restCycle)
+                     SpriteAnimation walkCycle,
+                     SpriteAnimation restCycle)
     {
         super(size, isImmobile, mass, health, walkCycle, restCycle);
+        Init();
     }
 
+    //Get Methods
+    public int GetTouchDamage()
+    {
+        return _touchDamage;
+    }
+
+    //Set Methods
+    public void SetTouchDamage(int _touchDamage)
+    {
+        this._touchDamage = _touchDamage;
+    }
+
+    //Public Methods
     @Override
     public void OnCollide(GameWorldObject other)
     {
@@ -29,5 +47,11 @@ public class EnemyBase extends CharacterBase
                         this.GetHealth() - projectile.GetDamage());
 
         }
+    }
+
+    //Private Methods
+    private void Init()
+    {
+        _touchDamage = 5;
     }
 }
