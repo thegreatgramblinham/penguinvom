24d02ecb369640c91721f79482b7b8d88b186f66
Sam <samhudock@gmail.com>
2016-03-03 21:22:52 -0500

    Simple dragon class.

---
+++ src/GameObjects/Characters/Enemies/Dagron.java
@@ -0,0 +1,16 @@
+package GameObjects.Characters.Enemies;
+
+import Animation.SpriteAnimation;
+
+import java.awt.*;
+
+public class Dagron extends EnemyBase
+{
+
+    public Dagron(Rectangle size, float mass, int health)
+    {
+        super(size, false, mass, health,
+                new SpriteAnimation("src/ImageAssets/enemies/blobSheet.png", 64, 64, 7, 60, true),
+                new SpriteAnimation("src/ImageAssets/enemies/blobSheet.png", 64, 64, 0, 60, true));
+    }
+}
