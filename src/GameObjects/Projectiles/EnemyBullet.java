c26898f96600bec8e417e46e913e6a769fbeb35a
Sam <Sam@localhost>
2016-03-05 14:14:03 -0500

    New class for enemy projectiles.

---
+++ src/GameObjects/Projectiles/EnemyBullet.java
@@ -0,0 +1,31 @@
+package GameObjects.Projectiles;
+
+import GameObjectBase.GameWorldObject;
+import GameObjects.Base.GameObject;
+
+import java.awt.*;
+import java.io.File;
+
+public class EnemyBullet extends ProjectileBase
+{
+    //Public Constants
+    public static final int HEIGHT = 16;
+    public static final int WIDTH = 16;
+
+    //Constructor
+    public EnemyBullet(Point p, GameObject owner)
+    {
+        super(new Rectangle(p.x, p.y, HEIGHT, WIDTH), 0.01F, owner, 5);
+        this.SetSprite(new javafx.scene.image.Image(
+                new File("src/ImageAssets/projectiles/enemyBullet0000.png")
+                        .toURI().toString()));
+        this.SetAlias("EnemyBullet");
+    }
+
+    @Override
+    public void OnCollide(GameWorldObject other)
+    {
+        this.SetNeedsDeletion(true);
+        super.OnCollide(other);
+    }
+}
