54072e16b40dfb1f56071f042b82bfb5bb53a653
Sam <Sam@localhost>
2016-03-31 18:35:49 -0400

    Creating GameConstants class to house button dictionary.

--- src/GameObjects/Characters/Enemies/Slim.java
+++ src/GameObjects/Characters/Enemies/Slim.java
@@ -3,6 +3,7 @@ package GameObjects.Characters.Enemies;
 import Animation.SpriteAnimation;
 import GameObjects.Characters.Enemies.AI.SlimAi;
 import GameObjects.Characters.Enemies.AI.enums.AiAction;
+import MainGame.GameConstants;
 import MainGame.GameManager;
 import javafx.scene.canvas.GraphicsContext;
 
@@ -63,8 +64,8 @@ public class Slim extends EnemyBase
                 new Rectangle(x + (int)this.GetHalfWidth() + 1,
                         y + (int)this.GetHalfHeight() + 10, 32, 32), 0.5F, 5);
 
-        GameManager.QueueObjectForAddition(s1, 3, GameManager.ENEMY_GROUP);
-        GameManager.QueueObjectForAddition(s2, 3, GameManager.ENEMY_GROUP);
+        GameManager.QueueObjectForAddition(s1, 3, GameConstants.ENEMY_GROUP);
+        GameManager.QueueObjectForAddition(s2, 3, GameConstants.ENEMY_GROUP);
 
         super.OnDeath();
     }
