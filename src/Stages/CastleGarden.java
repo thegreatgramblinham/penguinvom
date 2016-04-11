c3e92dbce7ee01be9ac79df512e6eb6a88711e37
Sam <Sam@localhost>
2016-04-11 19:15:43 -0400

    Moving creation of environmental objects to the base room class.

--- src/Stages/CastleGarden.java
+++ src/Stages/CastleGarden.java
@@ -2,17 +2,27 @@ package Stages;
 
 import GameObjectBase.enums.Side;
 import SectorBase.Sector;
+import javafx.scene.image.Image;
 
 import java.awt.*;
+import java.io.File;
 
 public class CastleGarden extends RoomBase
 {
+    //Private Constants
+    private final static Image SKY_TEXTURE = new Image(
+            new File("src/ImageAssets/backgrounds/skybg20000.png").toURI().toString());
+    private final static Image WALL_TEXTURE = new Image(
+            new File("src/ImageAssets/backgrounds/largeWallSector0000.png").toURI().toString());
+    private final static Image FLOOR_TEXTURE = new Image(
+            new File("src/ImageAssets/backgrounds/gardenGrassSector0000.png").toURI().toString());
+
     //Variables
 
     //Constructor
     public CastleGarden(Sector sector)
     {
-        super(sector);
+        super(sector, SKY_TEXTURE, WALL_TEXTURE, FLOOR_TEXTURE);
     }
 
     //Get Methods
@@ -23,16 +33,19 @@ public class CastleGarden extends RoomBase
     @Override
     public Point GetPlayerStartingLocation(Side s)
     {
+        switch(s)
+        {
+            case Top:
+            case Bottom:
+            case Left:
+            case Right:
+                return new Point(372, 372);
+        }
+
         return null;
     }
 
     //Private Methods
-    @Override
-    protected void InitBackdrop()
-    {
-
-    }
-
     @Override
     protected void InitStageBounds()
     {
