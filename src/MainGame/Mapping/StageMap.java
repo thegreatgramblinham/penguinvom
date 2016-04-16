c10004d540cf460442b9e5cd9559bb8c0eae3c13
Sam <samhudock@gmail.com>
2016-04-16 10:47:53 -0400

    Finishing stage switch implementation.

--- src/MainGame/Mapping/StageMap.java
+++ src/MainGame/Mapping/StageMap.java
@@ -1,5 +1,15 @@
 package MainGame.Mapping;
 
+import SectorBase.Sector;
+import SectorBase.enums.Direction;
+import Stages.CastleGardenStage;
+import Stages.MainCastleStage;
+import Stages.StageObject;
+
+import java.lang.reflect.Type;
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class StageMap
 {
     /*
@@ -7,20 +17,49 @@ public class StageMap
         **NOTE: This class has static dependencies, only one instance is allowed.**
     */
 
-    //Variables
+    //Private Variables
+    private static HashMap<Type, MapNode> _nodeLookup;
+
 
     //Constructor
+    public StageMap(Sector s1, Sector s2) throws Exception
+    {
+        _nodeLookup = new HashMap<>();
+        InitStageGroup1(s1, s2);
+    }
 
-    //Get Methods
+    //Public Static Methods
+    public static StageObject Query(Type t, Direction d)
+    {
+        if(!_nodeLookup.containsKey(t)) return null;
 
-    //Set Methods
+        MapNode n = _nodeLookup.get(t).GetLink(d);
 
-    //Public Methods
+        return n != null
+                ? n.GetStage()
+                : null;
+    }
+
+    public static StageObject Query(Type t)
+    {
+        if(!_nodeLookup.containsKey(t)) return null;
+
+        return _nodeLookup.get(t).GetStage();
+    }
 
     //Private Methods
-    private void InitStageGroup1()
+    private void InitStageGroup1(Sector s1, Sector s2) throws Exception
     {
+        HashSet<MapNode> nodeSet = new HashSet<>();
 
-    }
+        MapNode garden = new MapNode(new CastleGardenStage(s1));
+        MapNode castle = new MapNode(new MainCastleStage(s2));
+
+        garden.LinkDirection(castle, Direction.Right);
+        castle.LinkDirection(garden, Direction.Left);
+
+        _nodeLookup.put(CastleGardenStage.class, garden);
+        _nodeLookup.put(MainCastleStage.class, castle);
+;    }
 
 }
