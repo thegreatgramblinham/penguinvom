c7d649e152a28cc28ba1457439844c79b545c1c6
Sam Hudock <samhudock@gmail.com>
2016-04-29 16:23:36 -0400

    Stubbing out implementations of engine menus.

---
+++ src/Menus/Battle/BattleMenu.java
@@ -0,0 +1,15 @@
+package Menus.Battle;
+
+import Menus.MenuItem;
+
+import java.awt.*;
+
+public class BattleMenu extends MenuItem
+{
+
+    //Cin
+    public BattleMenu(Rectangle rect)
+    {
+        super(rect);
+    }
+}
