abdc46c3e2ea805597f5a8b8c76ab318298a7c29
Sam <Sam@localhost>
2016-05-01 11:46:59 -0400

    Creating the proper battle menu state machine.

--- src/Menus/Battle/BattleMenuManager.java
+++ src/Menus/Battle/BattleMenuManager.java
@@ -4,6 +4,7 @@ import MainGame.GameConstants;
 import MainGame.ViewPort;
 import Menus.Battle.AttackSubMenu.AttackMenu;
 import Menus.Battle.SelectionCarousel.BattleMenuCarousel;
+import Menus.Battle.enums.BattleMenuState;
 import Menus.Battle.enums.BattleMenuType;
 import Menus.MenuManager;
 import javafx.scene.input.KeyCode;
@@ -15,12 +16,14 @@ public class BattleMenuManager extends MenuManager
     //Variables
     private BattleMenuCarousel _battleCarousel;
     private AttackMenu _currentAttackMenu;
+    private BattleMenuState _state;
 
     //Constructor
     public BattleMenuManager()
     {
         super();
         InitCarousel();
+        _state = BattleMenuState.CarouselSelection;
     }
 
     //Get Methods
@@ -33,13 +36,47 @@ public class BattleMenuManager extends MenuManager
     {
         if(GameConstants.IsKeyPressed(KeyCode.UP))
         {
-            _battleCarousel.Advance();
+            switch(_state)
+            {
+                case CarouselSelection:
+                    _battleCarousel.Advance();
+                    break;
+                case SubMenuSelection:
+                    break;
+                case EnemySelection:
+                    break;
+            }
+
         }
         else if(GameConstants.IsKeyPressed(KeyCode.ENTER))
         {
-            BattleMenuType battleSelection = _battleCarousel.GetSelection();
-            //todo open the selection sub menu
-            InitSubMenu(battleSelection);
+            switch(_state)
+            {
+                case CarouselSelection:
+                    BattleMenuType battleSelection = _battleCarousel.GetSelection();
+                    InitSubMenu(battleSelection);
+                    _state = BattleMenuState.SubMenuSelection;
+                    break;
+                case SubMenuSelection:
+                    break;
+                case EnemySelection:
+                    break;
+            }
+        }
+        else if(GameConstants.IsKeyPressed(KeyCode.BACK_SPACE))
+        {
+            switch(_state)
+            {
+                case CarouselSelection:
+                    break;
+                case SubMenuSelection:
+                    RemoveMenu(_currentAttackMenu);
+                    _currentAttackMenu = null;
+                    _state = BattleMenuState.CarouselSelection;
+                    break;
+                case EnemySelection:
+                    break;
+            }
         }
 
     }
