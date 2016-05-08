63f2e3887e731247509a5b096ac4a359d57842f2
Sam <Sam@localhost>
2016-05-08 18:34:14 -0400

    Starting the text to image font coversion.

---
+++ src/Menus/Text/TextImager.java
@@ -0,0 +1,32 @@
+package Menus.Text;
+
+import javafx.scene.image.Image;
+
+import java.io.File;
+
+public class TextImager
+{
+    //Private Constants
+    private final static int START_INDEX = 2;
+    private final static int LETTER_HEIGHT = 16;
+
+    private final Image _fontImage = new Image(new File("src/ImageAssets/menus/text/pixelFont.png").toURI().toString());
+
+    //Variables
+
+    //Constructor
+
+    //Get Methods
+
+    //Set Methods
+
+    //Public Methods
+    public static Image TextToImage(String text) //todo wrap
+    {
+        return null;
+    }
+
+    //Private Methods
+
+
+}
