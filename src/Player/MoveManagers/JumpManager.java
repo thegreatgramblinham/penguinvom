ebcc870ca8c6929721c3a824be854d6cf8683fd4
Sam <samhudock@gmail.com>
2016-05-08 14:52:47 -0400

    Adding constants file for move xpaths.

--- src/Player/MoveManagers/JumpManager.java
+++ src/Player/MoveManagers/JumpManager.java
@@ -1,8 +1,12 @@
 package Player.MoveManagers;
 
-public class JumpManager
-{
+import Player.Moves.Move;
+import Player.Moves.MoveBuilder;
 
+import java.util.ArrayList;
+
+public class JumpManager extends MoveManager
+{
     //Variables
 
     //Constructor
@@ -12,6 +16,16 @@ public class JumpManager
     //Set Methods
 
     //Public Methods
+    @Override
+    public ArrayList<Move> GetMoveList() throws Exception
+    {
+        ArrayList<Move> moveList = new ArrayList<>();
+
+        Move m1 = MoveBuilder.BuildMove("src/config/moves/BasicJump.xml");
+        moveList.add(m1);
+
+        return moveList;
+    }
 
     //Private Methods
 
