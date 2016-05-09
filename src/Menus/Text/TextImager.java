a22a02c58bb17e698ab2cd868f1a13181bbae2e9
Sam <Sam@localhost>
2016-05-08 21:32:39 -0400

    Now have all the image metadata building into operational objects.

--- src/Menus/Text/TextImager.java
+++ src/Menus/Text/TextImager.java
@@ -1,32 +1,61 @@
 package Menus.Text;
 
+import XMLParsing.XMLParser;
 import javafx.scene.image.Image;
+import org.w3c.dom.Document;
 
+import java.awt.*;
 import java.io.File;
+import java.util.HashMap;
 
 public class TextImager
 {
     //Private Constants
+    private final static String FONT_FILEPATH = "src/ImageAssets/menus/text/pixelFont.png";
+    private final static String FONT_COORD_FILEPATH = "src/config/menus/font.xml";
+
     private final static int START_INDEX = 2;
     private final static int LETTER_HEIGHT = 16;
 
-    private final Image _fontImage = new Image(new File("src/ImageAssets/menus/text/pixelFont.png").toURI().toString());
+    private final Image _fontImage = new Image(new File(FONT_FILEPATH).toURI().toString());
 
     //Variables
+    private HashMap<Character, Point> _imageCoordinates;
 
     //Constructor
+    public TextImager()
+    {
+        Init();
+    }
 
     //Get Methods
 
     //Set Methods
 
     //Public Methods
-    public static Image TextToImage(String text) //todo wrap
+    public Image TextToImage(String text) //todo wrap
     {
         return null;
     }
 
     //Private Methods
+    private void Init()
+    {
+        _imageCoordinates = new HashMap<>();
+        Document d = XMLParser.CreateDocument(FONT_COORD_FILEPATH);
+
+        int nodeListLength = XMLParser.GetNodeListLength(d, "/Font/Symbol");
+
+        for(int i = 0; i < nodeListLength; i++)
+        {
+            String character
+                    = XMLParser.ParseStringPathContents(d, i,"/Font/Symbol/Character");
+            int x = XMLParser.ParseIntPathContents(d, i, "/Font/Symbol/X");
+            int y = XMLParser.ParseIntPathContents(d, i, "/Font/Symbol/Y");
+
+            _imageCoordinates.put(character.charAt(0), new Point(x, y));
+        }
+    }
 
 
 }
