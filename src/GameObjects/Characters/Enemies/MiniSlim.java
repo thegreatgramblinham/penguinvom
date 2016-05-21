2716a89bc19ba284f6b10f4453ba2140db046028
Sam Hudock <samhudock@gmail.com>
2016-05-21 14:45:26 -0400

    Added move execution event.

--- src/GameObjects/Characters/Enemies/MiniSlim.java
+++ src/GameObjects/Characters/Enemies/MiniSlim.java
@@ -13,7 +13,7 @@ public class MiniSlim extends EnemyBase
     //Private Constants
     private final static int WIDTH = 32;
     private final static int HEIGHT = 32;
-    private final static float INITIAL_MOVEMENT_SPEED = 1.0F;
+    private final static float INITIAL_MOVEMENT_SPEED = 0.5F;
 
     //Constructor
     public MiniSlim(Point location, float mass, int health)
