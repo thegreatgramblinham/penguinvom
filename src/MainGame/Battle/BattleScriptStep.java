ac23afb518771942651838d144e50f65ec11274e
Sam Hudock <samhudock@gmail.com>
2016-05-21 15:27:08 -0400

    Building up the necessary classes for battle animation scripting.

---
+++ src/MainGame/Battle/BattleScriptStep.java
@@ -0,0 +1,8 @@
+package MainGame.Battle;
+
+public class BattleScriptStep
+{
+    //TODO will serve as a container for each phase of a move animation
+    //eg
+    //attacker walk => attacker hit => defender recoil => attack return position
+}
---
+++ src/MainGame/Battle/MoveExecutor.java
@@ -0,0 +1,24 @@
+package MainGame.Battle;
+
+import javafx.scene.canvas.GraphicsContext;
+
+public final class MoveExecutor
+{
+    //Constructor
+    private MoveExecutor() {}
+
+    //Public Methods
+    private static void ExcuteMove(MoveExecutionEvent e, GraphicsContext gc)
+    {
+        // Check for halt conditions (e.g. touch attack on fire enemy)
+
+        // Calculate the distance that the attacker needs to move to.
+
+        // Using an animation script, move the attacker that distance
+        // within the time amount for the attack.
+
+        // Allowing for damage modifiers, inflict the damage from the incoming attack.
+
+        // Render the damage number animations.
+    }
+}
