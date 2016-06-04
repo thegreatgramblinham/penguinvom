88916999f48352f4cefbe0204738458b5990e778
Sam <samhudock@gmail.com>
2016-06-04 12:35:06 -0400

    Adding creation of scenery classes after property deserialization.

--- src/GameObjects/ImageConstants.java
+++ src/GameObjects/ImageConstants.java
@@ -5,13 +5,30 @@ import GameObjects.enums.ObjectCategory;
 public final class ImageConstants
 {
     private ImageConstants() {}
-    //Static Constants
-    private static String _SCENERY_ROOT = "src/ImageAssets/backgrounds/";
+    //Public Static Constants
+    public static String IMAGE_FILE_TYPE = ".png";
+    public static String SCENERY_ROOT = "src/imageAssets/backgrounds/";
+    public static String ENEMY_ROOT = "src/imageAssets/enemies/";
+    public static String PLAYER_ROOT = "src/imageAssets/player/";
+    public static String PROP_ROOT = "src/imageAssets/props/";
+    public static String PROJECTILE_ROOT = "src/imageAssets/projectiles/";
 
     //Public Methods
     public static String GetImageFilePathFromId(String Id, ObjectCategory category)
     {
-        return "";
+        switch(category)
+        {
+            case Backdrop:
+            case Floor:
+            case Wall:
+                return SCENERY_ROOT + Id + IMAGE_FILE_TYPE;
+            case Prop:
+                break;
+            case Enemy:
+                break;
+        }
+
+        return null;
     }
 
     //Private Methods
