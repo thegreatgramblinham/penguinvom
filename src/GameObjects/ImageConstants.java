677c8f1b5f1db33902b22446b2f302bf9fa37901
Sam <Sam@localhost>
2016-06-04 11:56:14 -0400

    Working on an image constants file that will make image paths easier to modify

---
+++ src/GameObjects/ImageConstants.java
@@ -0,0 +1,19 @@
+package GameObjects;
+
+import GameObjects.enums.ObjectCategory;
+
+public final class ImageConstants
+{
+    private ImageConstants() {}
+    //Static Constants
+    private static String _SCENERY_ROOT = "src/ImageAssets/backgrounds/";
+
+    //Public Methods
+    public static String GetImageFilePathFromId(String Id, ObjectCategory category)
+    {
+        return "";
+    }
+
+    //Private Methods
+
+}
